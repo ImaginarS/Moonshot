99135817db5eb9f6eb52da70ebb348c2cd8701a1
Sandra E Quel <sandra.quel0714@gmail.com>
2021-03-18 02:18:31 -0400

    Working app

---
+++ Moonshot/Astronaut.swif.swift
@@ -0,0 +1,12 @@
+//
+//  Astronaut.swif.swift
+//  Moonshot
+//
+//  Created by Sandra Q on 3/17/21.
+//
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
---
+++ Moonshot/ContentView.swift
@@ -0,0 +1,41 @@
+//
+//  ContentView.swift
+//  Moonshot
+//
+//  Created by Sandra Q on 3/17/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    let astronauts: [Astronaut] = Bundle.main.decode("astronauts.json")
+    let missions: [Mission] = Bundle.main.decode("missions.json")
+    
+    var body: some View {
+        NavigationView {
+            List(missions) { mission in
+                NavigationLink(
+                    destination: MissionView(mission: mission, astronauts: self.astronauts) )
+                {
+                        Image(mission.image)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 44, height: 44, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                    VStack(alignment: .leading) {
+                        Text(mission.displayName).font(.headline)
+                        Text(mission.formattedLaunchDate)
+
+                    }
+                    }
+                
+            }
+            .navigationBarTitle("Moonshot")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
